82abf43a01e04dcc47a6499e81ea8f7c069ac116
Josh Sutheran <josh.sutheran@gmail.com>
2022-05-16 12:48:29 +0100

    updated click feature

--- JS/Components/iconClickHandler.js
+++ JS/Components/iconClickHandler.js
@@ -1,15 +1,30 @@
-const headerIcons = document.querySelectorAll(".banner__language-icons")
+const socialLinks = document.querySelector('.info--bar__left').children;
+console.dir(socialLinks)
+
 
 const iconClick = function () {
-  Array.from(headerIcons).forEach((icon) => {
-    icon.addEventListener("mouseover", function () {
-      console.log(icon.src)
-      icon.src = `http://127.0.0.1:54186/Design/Imgs/${icon.id}--colour.png`
-      icon.classList.add("project__img--transform")
+  Array.from(socialLinks).forEach((icon) => {
+    icon.addEventListener('mouseover', function(event) {
+      icon.src = `./Design/Imgs/${icon.id}--colour.png`
+      icon.classList.add('info--bar__left--transform')
     })
-    icon.addEventListener("mouseout", function () {
-      icon.src = `http://127.0.0.1:54186/Design/Imgs/${icon.id}.png`
-      icon.classList.remove("project__img--transform")
+    icon.addEventListener('mouseout', function() {
+      icon.src = `./Design/Imgs/${icon.id}.png`
+      icon.classList.remove('info--bar__left--transform')
+    })
+    icon.addEventListener('click', function() {
+      if(icon.id === 'github--icon') {
+        window.open('https://github.com/joshsutheran')
+      }
+      if(icon.id === 'twitter--icon') {
+        window.open('https://twitter.com/joshsutheran')
+      }
+      if(icon.id === 'linkedin--icon') {
+        window.open('https://www.linkedin.com/in/josh-sutheran-04801b140/')
+      }
+      if(icon.id === 'mail--icon') {
+        document.location = 'mailto:josh.sutheran@gmail.com?subject=Front End Developer Role'
+      }
     })
   })
 }
