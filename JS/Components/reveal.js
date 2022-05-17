406021def0307555dc8a667fa9b386b6f8a01925
Josh Sutheran <josh.sutheran@gmail.com>
2022-05-17 16:43:06 +0100

    CSS reveal transition

---
+++ JS/Components/reveal.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".reveal");
+console.log(reveals);
+
+function reveal() {
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+export default reveal;
